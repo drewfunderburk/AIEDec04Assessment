b2a51e4224fa67ac148c2112ab5ae8483b4a2fcf
Drew <kevinfunderburkbusiness@gmail.com>
2020-11-13 08:54:05 -0600

    Set up Scene

--- AIEDec042020Assessment/AIEDec042020Assessment/Scene.cs
+++ AIEDec042020Assessment/AIEDec042020Assessment/Scene.cs
@@ -8,25 +8,152 @@ namespace AIEDec042020Assessment
     {
         public bool Started { get; set; }
 
+        private Actor[] _actors;
+
+        public Scene()
+        {
+            Started = false;
+            _actors = new Actor[0];
+        }
+
+        #region ACTOR METHODS
+        public void AddActor(Actor actor)
+        {
+            //Create a new array with a size one greater than our old array
+            Actor[] appendedArray = new Actor[_actors.Length + 1];
+            //Copy the values from the old array to the new array
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                appendedArray[i] = _actors[i];
+            }
+            //Set the last value in the new array to be the actor we want to add
+            appendedArray[_actors.Length] = actor;
+            //Set old array to hold the values of the new array
+            _actors = appendedArray;
+        }
+
+        public bool RemoveActor(int index)
+        {
+            //Check to see if the index is outside the bounds of our array
+            if (index < 0 || index >= _actors.Length)
+            {
+                return false;
+            }
+
+            bool actorRemoved = false;
+
+            //Create a new array with a size one less than our old array 
+            Actor[] newArray = new Actor[_actors.Length - 1];
+            //Create variable to access tempArray index
+            int j = 0;
+            //Copy values from the old array to the new array
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                //If the current index is not the index that needs to be removed,
+                //add the value into the old array and increment j
+                if (i != index)
+                {
+                    newArray[j] = _actors[i];
+                    j++;
+                }
+                else
+                {
+                    actorRemoved = true;
+                    if (_actors[i].Started)
+                        _actors[i].End();
+                }
+            }
+
+            //Set the old array to be the tempArray
+            _actors = newArray;
+            return actorRemoved;
+        }
+
+        public bool RemoveActor(Actor actor)
+        {
+            //Check to see if the actor was null
+            if (actor == null)
+            {
+                return false;
+            }
+
+            bool actorRemoved = false;
+            //Create a new array with a size one less than our old array
+            Actor[] newArray = new Actor[_actors.Length - 1];
+            //Create variable to access tempArray index
+            int j = 0;
+            //Copy values from the old array to the new array
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                if (actor != _actors[i])
+                {
+                    newArray[j] = _actors[i];
+                    j++;
+                }
+                else
+                {
+                    actorRemoved = true;
+                    if (actor.Started)
+                        actor.End();
+                }
+            }
+
+            //Set the old array to the new array
+            _actors = newArray;
+            //Return whether or not the removal was successful
+            return actorRemoved;
+        }
+
+        // Removes actors set to be removed
+        private void DestroyActors()
+        {
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                if (_actors[i].WillDestroy)
+                    RemoveActor(_actors[i]);
+            }
+        }
+        #endregion
+
         #region CORE
         public void Start()
         {
+            Started = true;
 
         }
 
         public void Update(float deltaTime)
         {
+            // Update all actors
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                if (!_actors[i].Started)
+                    _actors[i].Start();
 
+                _actors[i].Update(deltaTime);
+            }
+
+            // Clean up actors
+            DestroyActors();
         }
 
         public void Draw()
         {
-
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                _actors[i].Draw();
+            }
         }
 
         public void End()
         {
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                if (_actors[i].Started)
+                    _actors[i].End();
+            }
 
+            Started = false;
         }
         #endregion
     }
