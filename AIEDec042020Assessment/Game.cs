3dbcd6b2b57fbeb9ad1415403eb4c6e2c570acbd
Drew <kevinfunderburkbusiness@gmail.com>
2020-11-16 17:04:35 -0600

    Added drawing to Game

--- AIEDec042020Assessment/Game.cs
+++ AIEDec042020Assessment/Game.cs
@@ -7,15 +7,31 @@ namespace AIEDec042020Assessment
 {
     class Game
     {
-        public Scene scene;
+        public static Scene scene;
 
         public bool GameOver { get; set; }
-
 
         #region CORE
         private void Start()
         {
+            // Init Raylib window
+            Raylib.InitWindow(1024, 720, "AIEDec042020Assessment");
+            Raylib.SetTargetFPS(60);
+
+            // Create a new scene
             scene = new Scene();
+
+            // Spawn player in the center bottom of the screen
+            Player player = new Player(
+                Raylib.GetScreenWidth() / 2, 
+                Raylib.GetScreenHeight() * 0.95f, 
+                (float)Math.PI / 2);
+            player.Speed = 300;
+            scene.AddActor(player);
+
+            Enemy enemy = new Enemy(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() * 0.2f);
+            enemy.Target = player;
+            scene.AddActor(enemy);
         }
 
         private void Update(float deltaTime)
@@ -28,7 +44,12 @@ namespace AIEDec042020Assessment
 
         private void Draw()
         {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.RAYWHITE);
+
             scene.Draw();
+
+            Raylib.EndDrawing();
         }
 
         private void End()
